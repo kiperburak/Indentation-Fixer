2dc698834072ccd72daad16ea6a3d73f3b514ae2
kyperbeast <47918397+kyperbeast@users.noreply.github.com>
2020-11-22 20:31:56 +0300

    Final Commit

--- indentation.py
+++ indentation.py
@@ -1,7 +1,21 @@
+'''
+  INDENTATION FIXER
+  Burak Kiper 390626
+  Muhammed Halim Güleşen 348398
+'''
+
 from ArrayStack import *
 
 S = ArrayStack()
 space_count=0
+
+def reverse(stack):
+    items = []
+    while not stack.is_empty():
+        items.append(stack.pop())
+    for item in items:
+        stack.push(item)
+    return stack
 
 def find_bracket(line,space_count):
   if line.find('{') >= 0:
@@ -23,10 +37,12 @@ original = open(sfile)
 for line in original:
   l = line.rstrip('\n')
   space_count=find_bracket(l,space_count)
+
 original.close()
 
 tfile = "target.c"
-output = open(tfile, 'w')
+output = open(tfile, 'w') 
+reverse_stack=reverse(S)  
 while not S.is_empty():
-  output.write(S.pop() + '\n')  
+  output.write(reverse_stack.pop() + '\n') 
 output.close()
--- target.c
+++ target.c
@@ -1,24 +1,24 @@
-}
-    return 0;
-    }
-        }
-            printf("\n");
-        {
-        else
-        }
-            printf("%d",n);
-            }
-                printf("%d",i);
-            {
-            for(int j=0;j<n;j++)
-        {
-        if(n%2==0)
-        n = i*i;
-    {
-    for(i=2;i<5;i++)
-    i=0;
-    int i,n;
-    printf("Data Structures\n");
-{
-int main(void) 
 #include <stdio.h>
+int main(void) 
+{
+    printf("Data Structures\n");
+    int i,n;
+    i=0;
+    for(i=2;i<5;i++)
+    {
+        n = i*i;
+        if(n%2==0)
+        {
+            for(int j=0;j<n;j++)
+            {
+                printf("%d",i);
+            }
+            printf("%d",n);
+        }
+        else
+        {
+            printf("\n");
+        }
+    }
+    return 0;
+}
