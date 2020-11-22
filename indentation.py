0653904e50df0b5ab724c590d4776e60b61424b0
halimwastaken <halim_gulesen@hotmail.com>
2020-11-22 20:26:37 +0300

    finding bracket

--- indentation.py
+++ indentation.py
@@ -1,13 +1,28 @@
 from ArrayStack import *
 
 S = ArrayStack()
+space_count=0
+
+def find_bracket(line,space_count):
+  if line.find('{') >= 0:
+    new_line=space_count*' '+line
+    S.push(new_line)
+    space_count=space_count+4
+  elif line.find('}') >= 0:
+    space_count=space_count-4
+    new_line=space_count*' '+line
+    S.push(new_line)
+  else:
+    new_line=space_count*' '+line
+    S.push(new_line)
+  return space_count
 
 sfile = "source.c"
 original = open(sfile) 
     
 for line in original:
   l = line.rstrip('\n')
-  S.push(l)
+  space_count=find_bracket(l,space_count)
 original.close()
 
 tfile = "target.c"
